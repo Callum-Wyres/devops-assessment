f126f2f53a2982187ca4a648c162c8bfb3cb1866
Callum-Wyres <45015240+Callum-Wyres@users.noreply.github.com>
2020-02-13 20:13:01 +0000

    Update 4-appinsights.tf

--- terraform/4-appinsights.tf
+++ terraform/4-appinsights.tf
@@ -1,5 +1,5 @@
 resource "azurerm_application_insights" "appinsight" {
-  name                = "${local.prefix}-app-insights"
+  name                = "${local.prefix}app-insights"
   location            = local.location
   resource_group_name = local.resource_group
   application_type    = "Node.JS"
